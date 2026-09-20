d78236de6e920a07898e64bd98f089040d389a49
Gus Stoermer <gstoermer1@gmail.com>
2026-09-19 23:38:49 -0400

    feat: add basic memory modification functions

---
+++ include/memory.h
@@ -0,0 +1,6 @@
+#include "types.h"
+
+void *memcpy(void *dest, const void *src, uint32_t n);
+void *memset(void *dest, uint8_t c, uint32_t n);
+void *memmove(void *dest, const void *src, uint32_t n);
+
---
+++ src/memory.c
@@ -0,0 +1,36 @@
+#include "memory.h"
+
+void *memcpy(void *dest, const void *src, uint32_t n) {
+	uint8_t *d = dest;
+	const uint8_t *s = src;
+	while (n--) *d++ = *s++;
+	return dest;
+}
+
+void *memset(void *dest, uint8_t c, uint32_t n) {
+	uint8_t *d = dest;
+	while (n--) *d++ = c;
+	return dest;
+}
+
+void *memmove(void *dest, const void *src, uint32_t n) {
+	uint8_t *d = dest;
+	const uint8_t *s = (const char *)src;
+
+	if (d == s || n == 0) {
+		return dest;
+	}
+
+	if (d > s && d < s + n) {
+		for (uint32_t i = n; i > 0; i--) {
+			d[i - 1] = s[i - 1];
+		}
+	} else {
+		for (uint32_t i = 0; i < n; i++) {
+			d[i] = s[i];
+		}
+	}
+
+	return dest;
+}
+
